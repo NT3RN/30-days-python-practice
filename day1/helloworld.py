76ce87135c0cb08654e07554f53bf61a97f37647
S M Nafim Niloy <98659883+NT3RN@users.noreply.github.com>
2025-10-06 06:05:14 +0000

    day 1 practice file

---
+++ day1/helloworld.py
@@ -0,0 +1,14 @@
+print(3+2) #addition 
+print(3-2) #subtraction
+print(3*2) #multiplication
+print(3**2) #exponential 
+print(3 % 2) #modulus
+print(3 // 2) #floor division operator
+
+#checking datatype
+print(type(10))
+print(type(3.14))
+print (type(1 + 3j))
+print(type('Nafim Niloy'))
+print(type([1,2,3,4]))
+print(type({'name':'Nafim'}))
