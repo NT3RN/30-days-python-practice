d64aa48204d1369fae15e4f286b009dfd71abb36
nafim <98659883+NT3RN@users.noreply.github.com>
2025-10-17 23:34:50 +0600

    partial day 4

---
+++ 04_Day_Strings/day-4.py
@@ -0,0 +1,27 @@
+#Day 4 task 
+sentense1 = 'Thirty '+ 'Days '+'Of ' + 'Python'
+print(sentense1)
+
+sentense2 = 'Coding '+ 'For ' + 'All'
+print(sentense2)
+
+company = sentense2
+print(company)
+
+print(len(company))
+
+print(company.upper(), company.lower())
+print(company.capitalize())
+print(company.title())
+print(company.swapcase())
+print(company.strip('Coding'))
+print(company.replace('Coding', 'Python'))
+print(company.split())
+
+sentense3 = "Facebook, Google, Microsoft, Apple, Amazon"
+print(sentense3.split(','))
+
+print(company.find('C'))
+print(company.find('F'))
+print(company.rfind('l'))
+
