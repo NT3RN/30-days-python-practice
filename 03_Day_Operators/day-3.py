cb375c56e49c46f3d6ef844787ba6efc6bee2688
nafim <98659883+NT3RN@users.noreply.github.com>
2025-10-17 19:53:17 +0600

    day3 practice complete

--- 03_Day_Operators/day-3.py
+++ 03_Day_Operators/day-3.py
@@ -22,3 +22,38 @@ premiter = 2*(float(length)+float(width))
 area = float(length)*float(width)
 print('Area: ', area, 'Premiter: ', premiter)
 
+print(len('python'))
+print(len('dragon'))
+
+help('keywords')
+
+print('jargon' in 'I hope this course is not full of jargon')
+
+print(not 'on' in 'dragon' and 'on' in 'python')
+
+l = len('python')
+l=float(l)
+print(type(l))
+l=str(l)
+print(type(l))
+
+print(7//3 == int(2.7))
+i='9.8'
+i=float(i)
+i=int(i)
+print(type(i))
+print('10' == 10)
+print(i==10)
+print(i)
+
+hours = input('Enter hours : ')
+hours = int(hours)
+rate = input('Enter per hour rate: ')
+rate = int(rate)
+weeklyEarning = rate * hours
+print('Your weekly earning is', weeklyEarning, 'BDT')
+
+for i in range (1,6):
+    j = i*i
+    k =j*i
+    print(i, 1 , j, k)
