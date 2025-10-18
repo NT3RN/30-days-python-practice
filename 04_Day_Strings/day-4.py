debdacc98881dc4f525797dd062994f909d7db0b
nafim <98659883+NT3RN@users.noreply.github.com>
2025-10-18 17:59:04 +0600

    day 4 complete

--- 04_Day_Strings/day-4.py
+++ 04_Day_Strings/day-4.py
@@ -25,3 +25,45 @@ print(company.find('C'))
 print(company.find('F'))
 print(company.rfind('l'))
 
+sentense4 = 'You cannot end a sentense with because because is a conjuction'
+print(sentense4.find('because'))
+print(sentense4.rfind('because'))
+
+sentense5 = 'You cannot end a sentense with because because because is a conjuction'
+print(sentense5.replace('because ',''))
+
+print(company.startswith('Coding'))
+print(company.endswith('coding'))
+
+coding = ' Coding For All '
+print(coding.strip())
+
+challenge = '30DaysOfPython'
+print(challenge.isidentifier())
+challenge = 'thirty_days_of_python'
+print(challenge.isidentifier())
+
+pythonLibraries = ['Django', 'Flask', 'Bottle', 'Pyramid']
+result = '# '.join(pythonLibraries)
+print(result)
+
+block = 'I am enjoying this challenge.\nI just wonder what is next'
+print(block)
+
+tabEscape ='Name\tAge\tCountry\tCity\nNafim\t23\tBangladesh\tDhaka'
+print(tabEscape.expandtabs(10))
+
+radius = 10
+area = 3.14 * radius ** 2
+print('The area of a circle with radius {} is {} meters square'.format(radius, area))
+
+a = 8
+b = 6
+
+print('{} + {} = {}'.format(a, b, a + b))
+print('{} - {} = {}'.format(a, b, a - b))
+print('{} * {} = {}'.format(a, b, a * b))
+print('{} / {} = {:.2f}'.format(a, b, a / b))
+print('{} % {} = {}'.format(a, b, a % b))
+print('{} // {} = {}'.format(a, b, a // b))
+print('{} ** {} = {}'.format(a, b, a ** b))
