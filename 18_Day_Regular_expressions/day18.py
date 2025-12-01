39765a42dbaaf78bdb0dbe83dd14675dfd3a24f7
nafim <98659883+NT3RN@users.noreply.github.com>
2025-12-01 23:49:51 +0600

    having issue to solve day 18

---
+++ 18_Day_Regular_expressions/day18.py
@@ -0,0 +1,23 @@
+import re
+paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you  do not love something which can give you all the capabilities to develop an application what else can you love.'
+paragraph = paragraph.lower()
+plist = re.split(r'[. ]', paragraph)
+newPlist = []
+for i in plist:
+    if i.strip(): 
+        newPlist.append(i) 
+print(newPlist)
+plistSet = set(newPlist)
+print(plistSet)
+print(len(plistSet))
+a = 0
+outputFreq = 0
+outputWord = ''
+for w in plistSet:
+    match = re.findall(w, paragraph, re.I)
+    count = len(match)
+    if count > a:
+        outputFreq = count
+        outputWord = w
+
+print(outputWord,':', outputFreq)
